25f38efe35504bec2f87e22c26e43ac1777017fe
Damian Recoskie <damian_programmer@hotmail.com>
2022-09-13 20:41:18 -0400

    Update

--- UI.js
+++ UI.js
@@ -110,7 +110,7 @@ VHex.prototype.update = function( d )
       
       if( this.text )
       { 
-        val = !isNaN(val) ? val : 0x3F; if( val == 10 || val == 173 ) { val = 0x20; }
+        val = !isNaN(val) ? val : 0x3F; if( val == 0 || val == 10 || val == 173 ) { val = 0x20; }
 
         text += String.fromCharCode( val );
       }
